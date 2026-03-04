bd7740602c7561adaea52451936345ba24d7739b
Miguel Angel <mangellaura006@gmail.com>
2026-03-04 17:11:12 -0300

    Alterando o dasnboard do sistema para deixar ele com um visual e estrutura profissional. Arquivo novo criado para poder navegar entre os links da navegação

--- dashboard.php
+++ dashboard.php
@@ -32,7 +32,9 @@ $usuarios = $sql4->fetch_assoc()['ativos'] ?? 0;
 <body>
 
 <div class="sidebar">
-    <div class="logo"></div>
+    <div class="logo">
+        <img src="img/logo.png" alt="logo">
+    </div>
 
     <div class="menu">
         <a href="#">Dashboard</a>
